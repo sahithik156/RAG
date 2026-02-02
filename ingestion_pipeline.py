4da3a0008eb8f2b5f6da198fc571cabe75eae45c
sahithik156 <sahithik156@gmail.com>
2026-02-01 22:11:22 -0500

    commit 1

--- ingestion_pipeline.py
+++ ingestion_pipeline.py
@@ -7,3 +7,17 @@
 # Multiple documents -> made into chunks -> Use Embedding model to converts chunks into vectors. Vectors have dimensions (semantic knowledge about the chunk). -> Store vectors in vector database.
 # Retrieval pipeline:
 # User query -> USE THE SAME EMBEDDING MODEL AND THE # OF DIMENSIONS USED FOR INGESTION MODEL FOR THE USER QUERY -> user query vector -> Retriever gets the vectors from teh vector database that are closer to user query vector and returns their respective chunks -> chunks are summarized as an answer. 
+import os
+from langchain_community.document_loaders import TextLoader, DirectoryLoader # Used to read files from a directory.
+from langchain_text_splitters import CharacterTextSplitter # Used for creating chunks
+from langchain_openai import OpenAIEmbeddings # Embedding model 
+from langchain_chroma import Chroma # Vector database. Chroma can be hosted locally.
+from dotenv import load_dotenv # for environment variables
+
+load_dotenv()
+
+def main():
+    print("Main Function")
+
+if __name__ == "__main__":
+    main()
